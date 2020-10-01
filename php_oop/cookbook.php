cd0c579f66fc84c2080b5bdedd80f9353e472d24
miftah <70564481+miftachul10@users.noreply.github.com>
2020-10-01 10:56:12 +0700

    finish HPH_OOP

---
+++ php_oop/cookbook.php
@@ -0,0 +1,35 @@
+<?php
+include "classes/recipes.php";
+include "classes/render.php";
+include "classes/recipecollection.php";
+include "inc/recipes.php";
+
+$cookbook = new RecipeCollection("Teamtreehouse recipes");
+$cookbook->addRecipe($lemon_chicken);
+$cookbook->addRecipe($granola_muffins);
+$cookbook->addRecipe($belgian_waffles);
+$cookbook->addRecipe($pepper_casserole);
+$cookbook->addRecipe($lasagna);
+$cookbook->addRecipe($dried_mushroom_ragout);
+$cookbook->addRecipe($rabbit_catalan);
+$cookbook->addRecipe($grilled_salmon_with_fennel);
+$cookbook->addRecipe($pistachio_duck);
+$cookbook->addRecipe($chili_pork);
+$cookbook->addRecipe($crab_cakes);
+$cookbook->addRecipe($beef_medallions);
+$cookbook->addRecipe($silver_dollar_cakes);
+$cookbook->addRecipe($french_toast);
+$cookbook->addRecipe($corn_beef_hash);
+$cookbook->addRecipe($granola);
+$cookbook->addRecipe($spicy_omelette);
+$cookbook->addRecipe($scones);
+
+$breakfast = new RecipeCollection("Faforite Breakfast");
+foreach ($cookbook->filterByTag("breakfast") as $recipe){
+    $breakfast->addRecipe($recipe);
+}
+
+echo "\n\nSHOPPING_LIST\n\n";
+echo Render::listShoping($breakfast->getCombinedIngredients());
+
+// echo Render::displayRecipe($belgian_waffles);
